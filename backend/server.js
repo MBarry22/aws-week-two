4c177a8dfe26740a26c3e543c95b587adc3658f7
Mason Barry <mbarry22@my.bcit.ca>
2023-01-10 11:16:49 -0800

    dead

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,60 @@
-
-const mysqlDatabase = require('./mysqlDatabase')
-
-
-const express = require("express");
-const database = require("./database");
-
+const express = require('express');
+const path = require("path");
 const app = express();
 
-app.use(express.json())
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.get("/notes", (req, res) => {
-  const notes = database.getNotes();
-  res.send(notes);
-})
+    const notes = database.getNotes();
+    res.render("notes.ejs", {
+      notes,
+    });
+  })
+  
+  app.get("/notes/:id", (req, res) => {
+    const id = +req.params.id
+    const note = database.getNote(id)
+    if (!note) {
+      res.status(404).render("note404.ejs")
+      return
+    }
+  
+    res.render("singleNote.ejs", {
+      note,
+    });
+  })
+  
+  app.get("/createNote", (req, res) => {
+    res.render("createNote.ejs")
+  })
+  
+  app.post("/notes", (req, res) => {
+    const data = req.body
+    database.addNote(data)
+  
+    res.redirect("/notes");
+  })
+  
+  app.post("/notes/:id/delete", (req, res) => {
+    const id = +req.params.id
+    database.deleteNote(id)
+    res.redirect("/notes")
+  })
 
-app.get("/notes/:id", async (req, res) => {
-  const id = req.params.id
-  try {
-    const note = await mysqlDatabase.getNote(id)
-    res.send(note)
-  } catch (error) {
-    console.error(error)
-    res.sendStatus(500)
-  }
-})
+  app.get("/", (req, res) => {
+    res.render("index.ejs");
+    });
+    
+  
 
-
-app.post("/notes", (req, res) => {
-  const data = req.body
-  database.addNote(data)
-  res.status(201).send(data)
-})
-
-app.post("/notes/:id/delete", (req, res) => {
-  const id = +req.params.id
-  database.deleteNote(id)
-  res.send(id)
-})
-
-app.use(express.static("public"))
-
-const port = 8080;
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  
+  
+  const port = 8080;
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
